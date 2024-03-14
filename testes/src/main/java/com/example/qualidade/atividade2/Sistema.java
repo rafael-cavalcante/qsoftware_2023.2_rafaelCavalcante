c9a587bdaf1f8ad070036725271369f2451df476
Rafael Cavalcante <tecinf.rafaelcavalcante@gmail.com>
2024-03-13 21:34:26 -0300

    Update Sistema.java

--- testes/src/main/java/com/example/qualidade/atividade2/Sistema.java
+++ testes/src/main/java/com/example/qualidade/atividade2/Sistema.java
@@ -1,21 +1,21 @@
 package com.example.qualidade.atividade2;
 
 public class Sistema {
-    public String calcularImc(Usuario usuario, Nutricionista nutricionista){
-        
+    public String calcularImc(Usuario usuario, Nutricionista nutricionista) {
+
         if (usuario.getPeso() <= 0) {
             return "Valor inválido";
-        } else if(usuario.getAltura() <= 0){
+        } else if (usuario.getAltura() <= 0) {
             return "Valor inválido";
-        } 
+        }
 
-        if(usuario.getNome() != "" || nutricionista.getNome() != ""){
+        if (usuario.getNome() != "" || nutricionista.getNome() != "") {
             return "Campo em branco";
-        } else if(!usuario.getNome().contains(" ") || !nutricionista.getNome().contains(" ")){
+        } else if (!usuario.getNome().contains(" ") || !nutricionista.getNome().contains(" ")) {
             return "Valor inválido";
-        } 
-        
-        if(usuario.getCpf().length() != 11){
+        }
+
+        if (usuario.getCpf().length() != 11) {
             return "Valor inválido";
         }
 
@@ -25,23 +25,31 @@ public class Sistema {
 
         double imc = usuario.getPeso() / Math.pow(usuario.getAltura(), 2);
 
-/*
-IMC < 16: magreza grave
-Entre  16 e 17: magreza moderada
-Entre  17 e 19: magreza leve
-Entre  19 e 25: saudável
-Entre  25 e 30: sobrepeso
-Entre  30 e 35: obesidade grau 1
-Entre 35 e 40: obesidade grau 2
- */
+        /*
+         * IMC < 16: magreza grave
+         * Entre 16 e 17: magreza moderada
+         * Entre 17 e 19: magreza leve
+         * Entre 19 e 25: saudável
+         * Entre 25 e 30: sobrepeso
+         * Entre 30 e 35: obesidade grau 1
+         * Entre 35 e 40: obesidade grau 2
+         */
 
         if (imc < 16) {
             return "Magreza grave";
         } else if (imc >= 16 || imc < 17) {
-            return  "Magreza moderada";
+            return "Magreza moderada";
         } else if (imc >= 17 || imc < 19) {
             return "Magreza leve";
-        }       
+        } else if (imc >= 19 || imc < 25) {
+            return "Saudável";
+        } else if (imc >= 25 || imc < 30) {
+            return "Sobrepeso";
+        } else if (imc >= 30 || imc < 35) {
+            return "Obesidade grau 1";
+        } else if (imc >= 35 || imc < 40) {
+            return "Obesidade grau 2";
+        }
 
         return "Valor invalido";
     }
